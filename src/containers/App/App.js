b121420dd0b1db33f2b208bbfbbe130ccc66783a
Jake <jakeadmire1@gmail.com>
2019-03-28 14:21:40 -0600

    Import and implement Router into index.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { chooseFilm } from '../../actions';
+import { Route } from 'react-router-dom';
 
 import { fetchHappens } from '../../helpers/fetch';
 import { Crawl } from '../../components/Crawl/Crawl';
 import { Header } from '../../components/Header/Header';
+import { Main } from '../../components/Main/Main';
 
 export class App extends Component {
   constructor(props) {
@@ -19,7 +21,7 @@ export class App extends Component {
   }
 
   render() {
-    const { crawl, title, date, episode } = this.props.film;
+    const { crawl, title, date } = this.props.film;
     const crawlContent = { crawl, title, date };
     return (
       <div className="App">
