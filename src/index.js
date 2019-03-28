cf504bcceabdefb8d694009976122f95c3bd65f1
Jake <jakeadmire1@gmail.com>
2019-03-28 13:33:42 -0600

    import and implement Redux into index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.scss';
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers/index';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
