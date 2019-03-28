45929762dc890478a51dda73cce0513246d78bad
Jake <jakeadmire1@gmail.com>
2019-03-28 11:39:00 -0600

    Call fetchHappens to gather films and user rand num to select only one

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+import { fetchHappens } from '../../helpers/fetch';
+import { Crawl } from '../../components/Crawl/Crawl';
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = { film: {} };
+  }
+
+  componentDidMount() {
+    fetchHappens('https://swapi.co/api/films/')
+      .then(films => this.chooseFilm(films))
+      .catch(Error => this.setState({error: Error.message}) )
+  }
+
+  chooseFilm = (films) => {
+    const randNum = Math.floor(Math.random() * Math.floor(7));
+    let film = {
+      crawl: films[randNum].opening_crawl,
+      title: films[randNum].title,
+      date: films[randNum].release_date,
+      episode: films[randNum].episode_id
+    }
+    this.setState({film});
+  }
+
   render() {
+    const { crawl, title, date, episode } = this.state.film;
+    const crawlContent = { crawl, title, date };
+    console.log(crawl);
     return (
       <div className="App">
-      APP
+        <Crawl {...crawlContent}/>
       </div>
     );
   }
