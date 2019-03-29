55b37b9f2e4b61b7447e0da9a3e1a4561896d05e
Jake <jakeadmire1@gmail.com>
2019-03-29 08:07:10 -0600

    Write tests for mapStateToProps and mapDispatchToProps within App

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,6 +1,7 @@
 import { enzyme, shallow } from 'enzyme';
 import React from 'react'
-import { App } from './App';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
+import { chooseFilm } from '../../actions';
 
 describe('App', () => {
 
@@ -22,5 +23,34 @@ describe('App', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   }) 
+
+})
+
+describe('mapStateToProps', () => {
+
+  it('should return a film object and faves array as props', () => {
+    const mockState = {
+      film: {},
+      faves: []
+    }
+
+    const results = mapStateToProps(mockState)
+    expect(results).toEqual(mockState)
+  })
+
+})
+
+describe('mapDispatchToProps', () => {
+
+  it('should provide a method to dispatch an action creator', () => {
+    const mockFilms = [{}, {}];
+    const mockDispatch = jest.fn();
+    const actionToDispatch = chooseFilm(mockFilms);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+
+    mappedProps.chooseFilm(mockFilms);
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  })
 
 })
