49414752ec956f187e3392650d2f263bc33f5ef8
Jake <jakeadmire1@gmail.com>
2019-03-29 21:33:13 -0600

    Test default state of favoritesReducer

--- src/reducers/__tests__/favoritesReducer.js
+++ src/reducers/__tests__/favoritesReducer.js
@@ -3,8 +3,10 @@ import { favoritesReducer } from '../favoritesReducer';
 
 describe('chooseFilm', () => {
 
-  it('should', () => {
-
+  it('should return state by default', () => {
+    const state = [];
+    const action = {};
+    expect(favoritesReducer(state, action)).toEqual(state);
   })
 
 })
