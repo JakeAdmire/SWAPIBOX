cf9fa1153a31bebbc0f5991bd5fee56d88ebbfa8
Jake <jakeadmire1@gmail.com>
2019-03-29 21:42:18 -0600

    Test 'SET_CATEGORY' case of categoryReducer

--- src/reducers/__tests__/categoryReducer.js
+++ src/reducers/__tests__/categoryReducer.js
@@ -3,10 +3,22 @@ import { categoryReducer } from '../categoryReducer';
 
 describe('categoryReducer', () => {
 
+  it('should return the action event', () => {
+    const state = '';
+    const event = 'people';
+    const action = {
+      type: 'SET_CATEGORY',
+      event
+    };
+    const results = categoryReducer(state, action);
+    expect(results).toEqual(event);
+  })
+
   it('should return state by defualt', () => {
     const state = '';
     const action = {};
-    expect(categoryReducer(state, action)).toEqual(state);
+    const results = categoryReducer(state, action);
+    expect(results).toEqual(state);
   })
 
 })
