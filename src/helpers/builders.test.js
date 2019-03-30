9b4d506cf3a05446e55ecd42d4294a84539623e5
Jake <jakeadmire1@gmail.com>
2019-03-30 16:19:45 -0600

    Test vehiclesGatherer method within builders.js

--- src/helpers/builders.test.js
+++ src/helpers/builders.test.js
@@ -135,5 +135,44 @@ describe('builders', () => {
     }) 
     
   })
+
+  describe('vehiclesGatherer', () => {
+
+    it('should return the correct data', async () => {
+
+      let mockVehicle = {
+        cargo_capacity: 50000,
+        consumables: "2 months",
+        cost_in_credits: 150000,
+        created: "2014-12-10T15:36:25.724000Z",
+        crew: 46,
+        edited: "2014-12-22T18:21:15.523587Z",
+        films: [
+          "https://swapi.co/api/films/5/", 
+          "https://swapi.co/api/films/1/"
+          ],
+        length: 36.8,
+        manufacturer: "Corellia Mining Corporation",
+        max_atmosphering_speed: 30,
+        model: "Digger Crawler",
+        name: "Sand Crawler",
+        passengers: 30,
+        pilots: [],
+        url: "https://swapi.co/api/vehicles/4/",
+        vehicle_class: "wheeled"
+      }
+
+      const results = await vehiclesGatherer(mockVehicle);
+      const expected = {
+        "category": "vehicles", 
+        "model": "Digger Crawler", 
+        "name": "Sand Crawler", 
+        "passengers": 30, 
+        "vehicleClass": "wheeled"
+      };
+      expect(results).toEqual(expected);
+    })
+
+  })
   
 })
