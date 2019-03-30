16515bfef4659aa7b6ca2e93fb24c862677761b0
Jake <jakeadmire1@gmail.com>
2019-03-29 21:58:05 -0600

    Test peopleGatherer in builders.js

--- src/helpers/builders.test.js
+++ src/helpers/builders.test.js
@@ -6,8 +6,53 @@ import { fetchHappens } from './fetch';
 
 describe('builders', () => {
 
+  describe('peopleGatherer', () => {
+
+    it('should return the correct data', async () => {
+
+      let mockPerson = {
+        birth_year: "19BBY",
+        eye_color: "blue",
+        films: [
+          "https://swapi.co/api/films/2/",
+          "https://swapi.co/api/films/6/",
+          "https://swapi.co/api/films/3/",
+          "https://swapi.co/api/films/1/",
+          "https://swapi.co/api/films/7/"
+        ],
+        gender: "male",
+        hair_color: "blond",
+        height: "172",
+        homeworld: "https://swapi.co/api/planets/1/",
+        name: "Luke Skywalker",
+        skin_color: "fair",
+        species: ["https://swapi.co/api/species/1/"],
+        starships: [
+          "https://swapi.co/api/starships/12/",
+          "https://swapi.co/api/starships/22/"
+        ],
+        vehicles: [
+          "https://swapi.co/api/vehicles/14/",
+          "https://swapi.co/api/vehicles/30/"
+        ] 
+      }
+
+      const results = await peopleGatherer(mockPerson);
+      const expected = {
+        "category": "people",
+        "homeworld": "Tatooine",
+        "language": "Galactic Basic",
+        "name": "Luke Skywalker",
+        "population": "200000",
+        "species": "Human",
+      }
+      expect(results).toEqual(expected);
+    })
+
+  })
+
   describe('peopleCreator', () => {
-    
+
     let mockInfo = { 
       name: 'Bob',
       species: 'human',
