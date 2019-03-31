bfb8c507c8e0a8cd3c843ffe521c82eab34298d8
Jake <jakeadmire1@gmail.com>
2019-03-31 14:47:52 -0600

    Remove filmReducer from rootReducer method

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import { filmReducer } from "./filmReducer";
 import { favoritesReducer } from "./favoritesReducer";
 import { categoryReducer } from "./categoryReducer";
 
 export const rootReducer = combineReducers ({
-  film: filmReducer,
   faves: favoritesReducer,
   category: categoryReducer
 })
