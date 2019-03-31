5697644b001499a4ead7a96c1ea5469e8f71e956
Jake <jakeadmire1@gmail.com>
2019-03-31 14:49:54 -0600

    Replace changeCategory method with changeState to be more dynamic

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,46 +1,48 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { chooseFilm } from '../../actions';
 import { Route } from 'react-router-dom';
 
 import { fetchHappens } from '../../helpers/fetch';
 import { Crawl } from '../../components/Crawl/Crawl';
 import Header from '../../components/Header/Header';
-import Main from '../../components/Main/Main';
+import { Main } from '../../components/Main/Main';
 
 export class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      film: {},
+      error: '',
+      category: 'people',
+      renderCards: false
+    };
   }
 
   componentDidMount() {
-    fetchHappens('https://swapi.co/api/films/')
-      .then(films => this.props.chooseFilm(films.results))
+    const randNum = Math.floor(Math.random() * 7 + 1);
+    fetchHappens('https://swapi.co/api/films/' + randNum)
+      .then(film => this.setState({film}))
       .catch(Error => this.setState({error: Error.message}) )
   }
 
+  changeState = (state) => {
+    this.setState(state)
+  }
+
   render() {
-    const { crawl, title, date } = this.props.film;
-    const crawlContent = { crawl, title, date };
+    let crawlContent;
+    if (this.state.film) {
+      const { opening_crawl, title, release_date } = this.state.film;
+      crawlContent = { opening_crawl, title, release_date };
+    }
     return (
       <div className="App">
-        <Header />
+        <Header changeCategory={this.changeCategory} changeState={this.changeState}/>
         <Crawl {...crawlContent}/>
-        <Route path='/:id' render={ 
-          ({match}) => match.params.id && <Main />
-        }/>
+        <Route path='/:id' render={
+          ({match}) => <Main  category={this.state.category} 
+                              changeState={this.changeState}
+                              renderCards={this.state.renderCards}/>}/>
       </div>
     );
   }
 }
-
-export const mapStateToProps = (state) => ({
-  film: state.film
-})
-
-export const mapDispatchToProps = (dispatch) => ({
-  chooseFilm: (films) => dispatch(chooseFilm(films))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
