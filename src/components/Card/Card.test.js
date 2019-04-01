976a8dea4722ea37550fd1fa6d946c15daef87fa
Jake <jakeadmire1@gmail.com>
2019-04-01 07:54:01 -0600

    Test mapDispatchToProps within Card componenet

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,7 +1,8 @@
 import { enzyme, shallow } from 'enzyme';
 import React from 'react';
 
-import { Card, mapStateToProps } from './Card';
+import { Card, mapStateToProps, mapDispatchToProps } from './Card';
+import { addFavorite, removeFavorite } from '../../actions';
 import * as builders from '../../helpers/builders';
 
 describe('Card', () => {
@@ -32,6 +33,27 @@ describe('mapStateToProps', () => {
 
     const results = mapStateToProps(mockState)
     expect(results).toEqual(mockState)
+  })
+
+})
+
+describe('mapDispatchToProps', () => {
+
+  const mockFilm = {};
+  const mockDispatch = jest.fn();
+
+  it('should dispatch the addFavorite method', () => {
+    const actionToDispatch = addFavorite(mockFilm);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.addFavorite(mockFilm);
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  })
+
+  it('should dispatch the removeFavorite method', () => {
+    const actionToDispatch = removeFavorite(mockFilm);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.removeFavorite(mockFilm);
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
   })
 
 })
